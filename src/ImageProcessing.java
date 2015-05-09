25e3f38b91503a1a85525113c79b333698558d35
hooda hadad <hooda_fci@yahoo.com>
2015-05-10 01:26:46 +0200

    Auto associative memory class is created and ready to work but results is wrong, before fixing

---
+++ src/ImageProcessing.java
@@ -0,0 +1,60 @@
+// this code need to be fixed in readImage...It is just given to make it easy.
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+
+public class ImageProcessing {
+	
+
+	public static ArrayList<Integer> readImage(File inFile) {
+		
+		System.out.println("read: " + inFile);
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(inFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+		ArrayList<Integer> pixels = new ArrayList<Integer>();
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int rgb = image.getRGB(x, y);
+				int r = (int) ((rgb >> 16) & 0xff);
+				int g = (rgb >> 8) & 0xff;
+				int b = (rgb >> 0) & 0xff;
+				
+				if(r == 0)
+					pixels.add(0);
+				else
+					pixels.add(1);
+				
+			}
+		}
+
+		return pixels;
+	}
+	
+	////////////////////////////////////////
+	public static Vector<Integer>getNumberInVector(int n, int size)
+	{
+		
+		Vector<Integer>v = new Vector<Integer>();
+		v.setSize(size);
+		for(int i = size -1;i>=0;i--)
+		{
+			v.set(i, n%2);
+			n/=2;
+		}
+		
+		return v;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,153 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Vector;
+
+import Auto_Associative_Memory.AutoAssociativeMemory;
+
+
+
+public class Main {
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+
+		ArrayList <ArrayList<Integer> >images = getMemoryImages("Training");
+		if(images.size() ==0)
+		{
+			System.out.println("NO Training file");
+			return;
+		}
+
+		AutoAssociativeMemory auto = new AutoAssociativeMemory();
+		auto.m = images.get(0).size();
+
+		// add images to memory0
+		for (int i = 0; i < images.size(); i++) {
+			int []arr = toArray(images.get(i));
+			auto.Xs.add(arr);
+			auto.Ys.add(arr);
+
+		}
+		//////////////////
+		int choice;
+		while(true){
+			System.out.println("Choose one option");
+			System.out.println("1- Add to memory");
+			System.out.println("2- Initiate memory");
+			System.out.println("3- Recall X0");
+			System.out.println("4- View Energy");
+
+			choice = sc.nextInt();
+			if(choice == 1)
+			{
+				addToMemory();
+			}
+			else if(choice == 2)
+				AutoAssociativeMemory.initiateMemory();
+
+			else if(choice == 3)
+			{
+				File f;
+				while(true){
+					System.out.println("Enter x0 path");
+					String path = sc.next();
+					try{
+						f = new File(path);
+						if(f.exists())
+							break;
+						System.out.println("path not found");
+					}
+					catch(Exception e){
+						System.out.println("path not found");
+					}
+				}
+				
+				int []x0 = toArray(ImageProcessing.readImage(f)) ;
+				int [] image = AutoAssociativeMemory.reCallX0(x0);
+				int indx = searchInList(image, auto.Xs);
+				
+				int i = -1;
+				if(indx == 0){
+					i = 1;
+				}
+				else if(indx == 1){
+					i = 3;
+				}
+				else if(indx == 2){
+					i = 8;
+				}
+					
+				
+				System.out.println("Image number: " + i);
+			}
+
+			
+			else if(choice ==4)
+			{
+				AutoAssociativeMemory.viewEnergies();
+			}
+
+
+		}
+
+	}
+
+
+	///////////////////////////////////////////////////////////////////////////////
+	private static ArrayList <ArrayList<Integer> > getMemoryImages(String folderPath)
+	{
+		File folder = new File(folderPath);
+		ArrayList<ArrayList<Integer>>images = new ArrayList<ArrayList<Integer> >();
+
+		for(File f: folder.listFiles() )
+		{
+
+			ArrayList<Integer> i = ImageProcessing.readImage(f);
+			images.add(i);
+		}
+
+		return images;
+	}
+	private static int [] toArray(ArrayList<Integer>i)
+	{
+		int []arr = new int [i.size()];
+		for (int j = 0; j < arr.length; j++) {
+			arr[j] = i.get(j);
+		}
+		return arr;
+	}
+
+
+	private static void addToMemory(){
+		int []x = new int [AutoAssociativeMemory.m];
+
+		System.out.println("Enter x elements");
+
+		for (int i = 0; i < x.length; i++) {
+			x[i] = sc.nextInt();
+		}
+
+		AutoAssociativeMemory.Xs.add(x);
+		AutoAssociativeMemory.Ys.add(x);
+
+	}
+	//////////////////////////////////////////
+	public static int searchInList(int []arr, ArrayList<int []>list)
+	{
+		for (int i = 0; i < list.size(); i++) {
+			boolean equal = true;
+			for (int j = 0; j < arr.length; j++) {
+				if(arr[j]!=list.get(i)[j])
+				{
+					equal = false;
+					break;
+				}
+			}
+			if(equal)
+				return i;
+		}
+		
+		return -1;
+	}
+}
